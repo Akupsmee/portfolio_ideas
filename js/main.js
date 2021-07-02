c5172ef9d9b2a81fa808e06f1e102d13484b27a6
Akupsmee <akupuomeikenna@gmail.com>
2021-07-02 18:06:10 +0200

    Inital commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+const about = document.getElementById("about")
+const contact =  document.getElementById("contact")
+const aboutContent = document.getElementById("about-content")
+const contactContent = document.getElementById("contact-content")
+
+
+about.addEventListener('click', ()=>{
+    const aboutBox = new WinBox({
+        title : ' About Me',
+        modal : true,
+        background : '#00aa00',
+        width: '400px',
+        height: '400px',
+        top : 50,
+        right: 50,
+        bottom: 50,
+        left : 50,
+        mount: aboutContent,
+        onfocus: function(){
+            this.setBackground('#00aa00')
+        },
+        onblur: function(){
+            this.setBackground('#777')
+        },
+    })
+})
+
+contact.addEventListener('click', ()=>{
+    const contactBox = new WinBox({
+        title : ' Contact Me',
+        background : '#00aa00',
+        width: '400px',
+        height: '400px',
+        top : 150,
+        right: 50,
+        bottom: 50,
+        left : 250,
+        mount: contactContent,
+        onfocus: function(){
+            this.setBackground('#00aa00')
+        },
+        onblur: function(){
+            this.setBackground('#777')
+        },
+    })
+})
