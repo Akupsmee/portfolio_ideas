ce36d7d53002e101d3b8be383c1dc52dc5e45fd4
Akupsmee <akupuomeikenna@gmail.com>
2021-07-03 20:53:41 +0200

    responsiveness on mobile

--- js/main.js
+++ js/main.js
@@ -7,14 +7,13 @@ const contactContent = document.getElementById("contact-content")
 about.addEventListener('click', ()=>{
     const aboutBox = new WinBox({
         title : ' About Me',
-        modal : true,
         background : '#00aa00',
         width: '400px',
         height: '400px',
         top : 50,
         right: 50,
         bottom: 50,
-        left : 50,
+        left : 30,
         mount: aboutContent,
         onfocus: function(){
             this.setBackground('#00aa00')
@@ -25,16 +24,37 @@ about.addEventListener('click', ()=>{
     })
 })
 
+about.addEventListener('touchmove', ()=>{
+    const aboutBox = new WinBox({
+        title : ' About Me',
+        background : '#00aa00',
+        width: '400px',
+        height: '400px',
+        top : 50,
+        right: 50,
+        bottom: 50,
+        left : 30,
+        mount: aboutContent,
+        onfocus: function(){
+            this.setBackground('#00aa00')
+        },
+        onblur: function(){
+            this.setBackground('#777')
+        },
+    })
+})
+
+
 contact.addEventListener('click', ()=>{
     const contactBox = new WinBox({
         title : ' Contact Me',
         background : '#00aa00',
         width: '400px',
         height: '400px',
-        top : 150,
+        top : 350,
         right: 50,
         bottom: 50,
-        left : 250,
+        left : 80,
         mount: contactContent,
         onfocus: function(){
             this.setBackground('#00aa00')
@@ -44,3 +64,23 @@ contact.addEventListener('click', ()=>{
         },
     })
 })
+contact.addEventListener('touchmove', ()=>{
+    const contactBox = new WinBox({
+        title : ' Contact Me',
+        background : '#00aa00',
+        width: '400px',
+        height: '400px',
+        top : 350,
+        right: 50,
+        bottom: 50,
+        left : 80,
+        mount: contactContent,
+        onfocus: function(){
+            this.setBackground('#00aa00')
+        },
+        onblur: function(){
+            this.setBackground('#777')
+        },
+    })
+})
+
